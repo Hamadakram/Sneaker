8410be437a01ec9c5a83539241dd55de79e6bf68
Wynne <rwplaga@ncsu.edu>
2017-07-15 19:23:29 -0400

    Fix errors that prevent building.

--- app/src/main/java/com/irozon/sneakersample/MainActivity.java
+++ app/src/main/java/com/irozon/sneakersample/MainActivity.java
@@ -1,12 +1,10 @@
 package com.irozon.sneakersample;
 
-import android.graphics.Color;
 import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Toast;
 
 import com.irozon.sneaker.Sneaker;
 
